3825867bdd0ff398fab7fbaad9d62ded1bf20b1d
Claire <claire.xw205@gmail.com>
2020-05-24 16:38:51 +0800

    before tea time

--- src/components/Learn/index.js
+++ src/components/Learn/index.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import Nav from '../Nav'
+import thumbnail from '../../assets/videos/patrick-thumbnail.jpg';
+import ReactPlayer from 'react-player'
+import { Link } from "react-router-dom";
 
 export default function Learn() {
   return (
     <div>
       <Nav />
-      <br/>
+      <br />
+      <div className="container">
+        <div className="center-aligned row">
+          <div className="col s4"></div>
+          <div className="col s4">
+            <p className="white-text">
+              You'll be Patrick! Learn how to be patrick:
+            </p>
+          </div>
+          <div className="col s4"></div>
+
+        </div>
+        <div className="center-align row">
+          <div className="col s2"></div>
+          <div className="col s8">
+            <ReactPlayer className='' url='https://www.youtube.com/watch?v=rMog3TXQRds' />
+          </div>
+          <div className="col s2"></div>
+        </div>
+        <div className="center-align row">
+          <Link to='/selectLanguage' className="waves-effect waves-light cyan btn-large">I'm Ready!</Link>
+        </div>
+        {/*
+      mute 
+      <ReactPlayer url='https://youtu.be/Vd-CYZ6Sf20' /> */}
+      </div>
     </div>
   )
 }
