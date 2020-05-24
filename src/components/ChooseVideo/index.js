943ca7aef91ece42df606c21b43e481cfa3980c9
Claire <claire.xw205@gmail.com>
2020-05-24 22:57:46 +0800

    last

--- src/components/ChooseVideo/index.js
+++ src/components/ChooseVideo/index.js
@@ -13,11 +13,24 @@ export default function ChooseVideo() {
       <div className="container">
         <h5 className="center-align white-text">Select Video</h5>
         <br />
+        <div className="center-align row">
+          <div className="col s12">
+            <input type="text" placeholder='Search ...' />
+          </div>
+        </div>
         <div className="center-align row" >
           <div className="col s4">
+          <div className="card" onClick={e=>alert('Coming Soon!')}>
+              <div className="card-image waves-effect waves-block waves-light ">
+                <img src={require('../../assets/videos/nani.jpg')} />
+              </div>
+              <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">TED Talk - 8 secrets of success</span>
+              </div>
+            </div>
           </div>
           <div className="col s4">
-            <div className="card" onClick={e=>history.push('/learn')}>
+            <div className="card" onClick={e => history.push('/learn')}>
               <div className="card-image waves-effect waves-block waves-light ">
                 <img src={thumbnail} />
               </div>
@@ -27,6 +40,14 @@ export default function ChooseVideo() {
             </div>
           </div>
           <div className="col s4">
+            <div className="card" onClick={e => alert('Coming Soon!')}>
+              <div className="card-image waves-effect waves-block waves-light ">
+                <img src={require('../../assets/videos/sheldon.jpg')} />
+              </div>
+              <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">The Big Bang Theory - Pictionary</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
